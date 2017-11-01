c0c2226012a8dd319d2ed963a722dc44bdabe572
MikeFord12 <fordmich@mail.gvsu.edu>
2017-11-01 14:55:14 -0400

    Add files via upload

--- FileTypeLogger.py
+++ FileTypeLogger.py
@@ -7,12 +7,25 @@ import time
 fileTypes = {
 
 }
+userPath = raw_input("Enter in desired file path: ")
+
 timeElapsed = time.time()
 
-rootDir = "C:\\Engineering"
-
-for dirName, subdirList, fileList in os.walk(rootDir):
-    print "Found Directory: %s" % dirName
+for dirName, subdirList, fileList in os.walk(userPath):
+    if '.svn' in subdirList:
+        subdirList.remove('.svn')
     for fName in fileList:
-        print "Found File: %s" % fName
-print time.time() - timeElapsed
+        extension = os.path.splitext(fName)[1].lower()
+        if extension in fileTypes:
+            fileTypes[extension] += 1
+        elif extension == "":
+            fileTypes[fName] = 1
+        else:
+            fileTypes[extension] = 1
+
+logFile = open("log.txt","w")
+
+
+for elements in fileTypes:
+    logFile.write("%s: %d\n" % (elements, fileTypes[elements]))
+logFile.write("Time Elapsed: %.2fs" % (time.time() - timeElapsed))
